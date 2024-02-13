862cc4f40e12cce2c56816632fbcf617801e4f0e
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2024-02-13 15:19:53 -0800

    Move sample app to this repro.

---
+++ src/WinFormsDx/D2DTestApp/D2DSamples/DirectWriteTestForm.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms.Direct2D;
+
+namespace D2DTestApp;
+
+public partial class DirectWriteTestForm : Form
+{
+    public DirectWriteTestForm()
+    {
+        InitializeComponent();
+    }
+
+    private void direct2dPanel1_PaintIGraphics(object sender, GraphicsPaintEventArgs e)
+    {
+        e.Graphics.Clear(BackColor);
+        e.Graphics.DrawString("TestString", Font, Brushes.Black, 50, 50);
+        e.Graphics.DrawString("TestString 2", Font, Brushes.Black, 50, 70);
+
+        var size = e.Graphics.MeasureString("TestString", Font, ClientSize);
+    }
+}
--- src/WinFormsDx/D2DTestApp/Program.cs
+++ src/WinFormsDx/D2DTestApp/Program.cs
@@ -11,7 +11,7 @@ namespace D2DTestApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmMain());
+            Application.Run(new Direct2DTestForm());
         }
     }
 }
