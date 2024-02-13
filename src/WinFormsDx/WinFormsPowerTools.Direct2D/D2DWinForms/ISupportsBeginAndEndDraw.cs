88025a24e9cc7eb40df03c40dc30d23fe526a4b8
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2024-02-13 13:24:14 -0800

    Initial Solution checkin.

---
+++ src/WinFormsDx/WinFormsPowerTools.Direct2D/D2DWinForms/ISupportsBeginAndEndDraw.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace System.Windows.Forms.Direct2D
+{
+    public interface ISupportsBeginAndEndDraw
+    {
+        void BeginDraw();
+        void Clear(Color color);
+        void EndDraw();
+    }
+}
